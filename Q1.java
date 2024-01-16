118fc07e134f8792578cc5809026c6203640c42c
Rohan Kumar Behera <118833348+rkb1930@users.noreply.github.com>
2024-01-17 01:24:34 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,27 @@
+package list;
+
+import java.util.Arrays;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 55, 6};
+        bubbleSort(arr);
+
+        // Print sorted array
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 1; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
